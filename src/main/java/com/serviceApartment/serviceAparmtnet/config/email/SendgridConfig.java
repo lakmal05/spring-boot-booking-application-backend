f9501858fa1dad9a9d2a69c29c53dec79e61cb67
Lakmal05 <lakmal@webmotech.com>
2024-09-04 16:35:36 +0530

    sendgrid service

--- src/main/java/com/serviceApartment/serviceAparmtnet/config/email/SendgridConfig.java
+++ src/main/java/com/serviceApartment/serviceAparmtnet/config/email/SendgridConfig.java
@@ -1,7 +1,42 @@
 package com.serviceApartment.serviceAparmtnet.config.email;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SendgridConfig {
+
+    @Value("${sendgrid.api.key}")
+    private String sendGridApiKey;
+
+    public void sendEmail(String toEmail, String message) {
+        Email from = new Email("your-email@example.com");
+        Email to = new Email(toEmail);
+        String subject = "Subject Here";
+        Content content = new Content("text/plain", message);
+        Mail mail = new Mail(from, subject, to, content);
+
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+
+            SendGrid sendGrid = new SendGrid(sendGridApiKey);
+            Response response = sendGrid.api(request);
+
+            System.out.println("Response Status Code: " + response.getStatusCode());
+            System.out.println("Response Body: " + response.getBody());
+            System.out.println("Response Headers: " + response.getHeaders());
+        } catch (Exception ex) {
+            System.out.println(ex + " Error sending email");
+        }
+    }
 }
--- src/main/java/com/serviceApartment/serviceAparmtnet/service/EmailService.java
+++ src/main/java/com/serviceApartment/serviceAparmtnet/service/EmailService.java
@@ -1,20 +1,22 @@
 package com.serviceApartment.serviceAparmtnet.service;
 
+import com.serviceApartment.serviceAparmtnet.config.email.SendgridConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
+    private final SendgridConfig sendGridConfig;
 
+    @Autowired
+    public EmailService(SendgridConfig sendGridConfig) {
+        this.sendGridConfig = sendGridConfig;
+    }
 
-//    public String sendUserCrdentials(String email, String message) {
-//
-//        var data={
-//        email:email,
-//        message:message
-//        }
-//        //this.sendGridService.sendEmail(data)
-//
-//    }
+    public String sendUserCredentials(String email, String message) {
+        sendGridConfig.sendEmail(email, message);
+        return "Email sent successfully";
+    }
 
 }
